b0315a64f12198b1299df943424fbc0cf4381ea4
Dani C R <danielcr86@gmail.com>
2018-11-17 12:50:23 +0100

    Add $(document).ready placeholder
    Load jquery in the HTML so the $(document).ready works properly

--- src/main/resources/static/new_shipment_form_functions.js
+++ src/main/resources/static/new_shipment_form_functions.js
@@ -1,9 +1,6 @@
-var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = "http://code.jquery.com/jquery-3.3.1.min.js";
+$( document ).ready(function() {
+});
 
-head.appendChild(script)
 
 function enableOrDisableSubmitButton() {
     var submitAllowed = true;
