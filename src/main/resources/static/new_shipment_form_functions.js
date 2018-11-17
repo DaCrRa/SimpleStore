f7b835acd2c910543506aea2addc5aa1cf95a2c4
Dani C R <danielcr86@gmail.com>
2018-11-17 12:50:25 +0100

    Refactor form functions javascript
    Extract logic to adjust indexes of items array to its own function.
    Extract logic to enable of disable Remove buttons to its own function.
    Invoke these functions, and also the one for enabling/disabling submit
    buttons, whenever the form is loaded.
    
    This will be useful to reuse the same form and functions to edit a
    shipment: the fields in the form will not be empty when loading the
    page, therefore we will need to run the logic on document ready

--- src/main/resources/static/new_shipment_form_functions.js
+++ src/main/resources/static/new_shipment_form_functions.js
@@ -1,6 +1,17 @@
 $( document ).ready(function() {
+	enableOrDisableSubmitButton();
+	enableOrDisableRemoveButtons();
+	adjustItemsIndexes();
 });
 
+function enableOrDisableRemoveButtons() {
+	var removeButtons = $("button.removeItemField");
+	if( removeButtons.length > 1) {
+		removeButtons.removeAttr("disabled");
+	} else {
+		removeButtons.attr("disabled", "true");
+	}
+}
 
 function enableOrDisableSubmitButton() {
     var submitAllowed = true;
@@ -16,33 +27,35 @@ function enableOrDisableSubmitButton() {
     }
 }
 
+function adjustItemsIndexes() {
+    $("span.itemFieldText").each(function(index) {
+        $(this).text("Item " + (index + 1) + ":");
+    });
+    $("button.removeItemField").each(function(index, element) {
+        element.setAttribute("onclick", "onRemoveItemClicked(" + (index) +")");
+    });
+    $("input.itemFieldInput").each(function(index, element) {
+        element.setAttribute("name", "items[" + (index) + "].name");
+    });
+}
+
 function onAddItemClicked() {
     var itemFields = document.getElementById("itemFields");
     var lastItemField = itemFields.lastElementChild;
-    var lastItemIndex = parseInt(lastItemField.getElementsByClassName("itemFieldInput")[0].getAttribute("name").match(/[0-9]+/));
-    var newItemIndex = lastItemIndex + 1;
     var newItemField = lastItemField.cloneNode(true);
-    newItemField.getElementsByClassName("itemFieldText")[0].firstChild.nodeValue = "Item " + (newItemIndex + 1) + ":";
-    newItemField.getElementsByClassName("itemFieldInput")[0].setAttribute("name", "items[" + newItemIndex + "].name");
-    newItemField.getElementsByClassName("itemFieldInput")[0].value = "";
-    newItemField.getElementsByClassName("removeItemField")[0].setAttribute("onclick", "onRemoveItemClicked(" + newItemIndex +")"); 
     itemFields.appendChild(newItemField);
-    $("button.removeItemField").removeAttr("disabled");
+    newItemField.getElementsByClassName("itemFieldInput")[0].value = "";
+    adjustItemsIndexes();
+    enableOrDisableRemoveButtons();
     enableOrDisableSubmitButton();
 }
 
 function onRemoveItemClicked(itemIndex) {
     var itemFieldsContainer = document.getElementById("itemFields");
     var itemFields = itemFieldsContainer.children;
-    for(i = itemIndex+1 ; i < itemFields.length ; i++) {
-        itemFields[i].getElementsByClassName("itemFieldText")[0].firstChild.nodeValue = "Item " + i + ":";
-        itemFields[i].getElementsByClassName("itemFieldInput")[0].setAttribute("name", "items[" + (i - 1) + "].name");
-        itemFields[i].getElementsByClassName("removeItemField")[0].setAttribute("onclick", "onRemoveItemClicked(" + (i - 1) +")");
-    }
     itemFieldsContainer.removeChild(itemFields[itemIndex]);
-    if (itemFields.length == 1) {
-        $("button.removeItemField").attr("disabled", "true");
-    }
+    adjustItemsIndexes();
+    enableOrDisableRemoveButtons();
 }
 
 function onType() {
