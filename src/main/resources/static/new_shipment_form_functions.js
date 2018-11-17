5be5b7e62a468d70a79d526a72c1a6dbe87619fa
Dani C R <danielcr86@gmail.com>
2018-11-17 12:50:09 +0100

    Add the jquery script from our js file
    In order to not expose it into the HTML file...

--- src/main/resources/static/new_shipment_form_functions.js
+++ src/main/resources/static/new_shipment_form_functions.js
@@ -1,3 +1,10 @@
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "http://code.jquery.com/jquery-3.3.1.min.js";
+
+head.appendChild(script)
+
 function onAddItemClicked() {
     var itemFields = document.getElementById("itemFields");
     var lastItemField = itemFields.lastElementChild;
