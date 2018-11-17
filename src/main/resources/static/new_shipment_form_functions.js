754f7d66230f5ec8f76769db16f7610fd973724c
Dani C R <danielcr86@gmail.com>
2018-11-17 12:50:07 +0100

    Add remove buttons to item fields
    Added "Remove" button next to each field.
    
    Added function to define the behavior. It receives the index
    of the item to remove. Since the array of items has to be consistent,
    the function loops through the next items in the array, decreasing
    their index. The index shows up in the field text, in the name attribute
    of the input element, and in the parameter of the onclick function
    of the remove element.
    
    Renamed the javascript file to something more accurate.

---
+++ src/main/resources/static/new_shipment_form_functions.js
@@ -0,0 +1,23 @@
+function onAddItemClicked() {
+    var itemFields = document.getElementById("itemFields");
+    var lastItemField = itemFields.lastElementChild;
+    var lastItemIndex = parseInt(lastItemField.getElementsByClassName("itemFieldInput")[0].getAttribute("name").match(/[0-9]+/));
+    var newItemIndex = lastItemIndex + 1;
+    var newItemField = lastItemField.cloneNode(true);
+    newItemField.getElementsByClassName("itemFieldText")[0].firstChild.nodeValue = "Item " + (newItemIndex + 1) + ":";
+    newItemField.getElementsByClassName("itemFieldInput")[0].setAttribute("name", "items[" + newItemIndex + "].name");
+    newItemField.getElementsByClassName("itemFieldInput")[0].value = "";
+    newItemField.getElementsByClassName("removeItemField")[0].setAttribute("onclick", "onRemoveItemClicked(" + newItemIndex +")"); 
+    itemFields.appendChild(newItemField);
+}
+
+function onRemoveItemClicked(itemIndex) {
+    var itemFieldsContainer = document.getElementById("itemFields");
+    var itemFields = itemFieldsContainer.children;
+    for(i = itemIndex+1 ; i < itemFields.length ; i++) {
+        itemFields[i].getElementsByClassName("itemFieldText")[0].firstChild.nodeValue = "Item " + i + ":";
+        itemFields[i].getElementsByClassName("itemFieldInput")[0].setAttribute("name", "items[" + (i - 1) + "].name");
+        itemFields[i].getElementsByClassName("removeItemField")[0].setAttribute("onclick", "onRemoveItemClicked(" + (i - 1) +")");
+    }
+    itemFieldsContainer.removeChild(itemFields[itemIndex]);
+}
