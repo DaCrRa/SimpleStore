6082c169f310581063fa0add2d1029fded3f96ac
Dani C R <danielcr86@gmail.com>
2018-11-17 12:49:47 +0100

    Show shipment detail
    Added URL mapping to show one shipment.
    The controller recovers the shipment from the ShipmentRepository.
    Added mustache template, which iterates the shipment items, or
    shows "no items" message

--- src/main/java/es/danielcr86/simpleStore/ShipmentsController.java
+++ src/main/java/es/danielcr86/simpleStore/ShipmentsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ShipmentsController {
@@ -22,6 +23,13 @@ public class ShipmentsController {
 	public String listShipments(Model model) {
 		model.addAttribute("shipments", shipmentRepository.findAll());
 		return "shipment_list";
+	}
+
+	@GetMapping("/shipment/{id}")
+	public String showShipment(Model model, @PathVariable Long id) {
+		Shipment shipmentToShow = shipmentRepository.findById(id).get();
+		model.addAttribute("shipment", shipmentToShow);
+		return "shipment_detail";
 	}
 
 }
