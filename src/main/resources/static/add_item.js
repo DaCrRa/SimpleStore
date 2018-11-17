e5e5166fd48667e26112fbcf3e0e4c1e7efa6ad9
Dani C R <danielcr86@gmail.com>
2018-11-17 12:50:06 +0100

    Get the index for the new item from the last one in the form
    Avoid the need of maintaining a global counter.

--- src/main/resources/static/add_item.js
+++ src/main/resources/static/add_item.js
@@ -1,8 +1,8 @@
-var counter=0;
 function onAddItemClicked() {
-    var newItemIndex = ++counter;
     var itemFields = document.getElementById("itemFields");
     var lastItemField = itemFields.lastElementChild;
+    var lastItemIndex = parseInt(lastItemField.getElementsByClassName("itemFieldInput")[0].getAttribute("name").match(/[0-9]+/));
+    var newItemIndex = lastItemIndex + 1;
     var newItemField = lastItemField.cloneNode(true);
     newItemField.getElementsByClassName("itemFieldText")[0].firstChild.nodeValue = "Item " + (newItemIndex + 1) + ":";
     newItemField.getElementsByClassName("itemFieldInput")[0].setAttribute("name", "items[" + newItemIndex + "].name");
