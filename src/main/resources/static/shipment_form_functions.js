ecf4d803321613e857ad74c826c753df0ab395be
Dani C R <danielcr86@gmail.com>
2018-11-17 12:50:42 +0100

    Inofensive format adjustment

--- src/main/resources/static/shipment_form_functions.js
+++ src/main/resources/static/shipment_form_functions.js
@@ -14,58 +14,58 @@ function enableOrDisableRemoveButtons() {
 }
 
 function enableOrDisableSubmitButton() {
-    var submitAllowed = true;
-    $("input.required").each(function(idx, element) {
-        if (element.value.length == 0) {
-            submitAllowed = false;
-        }
-    });
-    if (submitAllowed) {
-        $("#submitButton").removeAttr("disabled");
-    } else {
-        $("#submitButton").attr("disabled", "true");
-    }
+	var submitAllowed = true;
+	$("input.required").each(function(idx, element) {
+		if (element.value.length == 0) {
+			submitAllowed = false;
+		}
+	});
+	if (submitAllowed) {
+		$("#submitButton").removeAttr("disabled");
+	} else {
+		$("#submitButton").attr("disabled", "true");
+	}
 }
 
 function adjustItemsIndexes() {
-    $(".itemFieldText").each(function(index) {
-        $(this).text("Item " + (index + 1));
-    });
-    $("button.removeItemField").each(function(index, element) {
-        element.setAttribute("onclick", "onRemoveItemClicked(" + (index) +")");
-    });
-    $("input.itemFieldInput").each(function(index, element) {
-        element.setAttribute("name", "items[" + (index) + "].name");
-    });
-    $("input.itemId").each(function(index, element) {
-        element.setAttribute("name", "items[" + (index) + "].id");
-    });
+	$(".itemFieldText").each(function(index) {
+		$(this).text("Item " + (index + 1));
+	});
+	$("button.removeItemField").each(function(index, element) {
+		element.setAttribute("onclick", "onRemoveItemClicked(" + (index) +")");
+	});
+	$("input.itemFieldInput").each(function(index, element) {
+		element.setAttribute("name", "items[" + (index) + "].name");
+	});
+	$("input.itemId").each(function(index, element) {
+		element.setAttribute("name", "items[" + (index) + "].id");
+	});
 }
 
 function onAddItemClicked() {
-    var itemFields = document.getElementById("itemFields");
-    var lastItemField = itemFields.lastElementChild;
-    var newItemField = lastItemField.cloneNode(true);
-    itemFields.appendChild(newItemField);
-    newItemField.getElementsByClassName("itemFieldInput")[0].value = "";
-    adjustItemsIndexes();
-    var itemIdInputToRemove = newItemField.getElementsByClassName("itemId");
-    if (itemIdInputToRemove.length > 0) {
-        newItemField.removeChild(newItemField.getElementsByClassName("itemId")[0]);
-    }
-    enableOrDisableRemoveButtons();
-    enableOrDisableSubmitButton();
+	var itemFields = document.getElementById("itemFields");
+	var lastItemField = itemFields.lastElementChild;
+	var newItemField = lastItemField.cloneNode(true);
+	itemFields.appendChild(newItemField);
+	newItemField.getElementsByClassName("itemFieldInput")[0].value = "";
+	adjustItemsIndexes();
+	var itemIdInputToRemove = newItemField.getElementsByClassName("itemId");
+	if (itemIdInputToRemove.length > 0) {
+		newItemField.removeChild(newItemField.getElementsByClassName("itemId")[0]);
+	}
+	enableOrDisableRemoveButtons();
+	enableOrDisableSubmitButton();
 }
 
 function onRemoveItemClicked(itemIndex) {
-    var itemFieldsContainer = document.getElementById("itemFields");
-    var itemFields = itemFieldsContainer.children;
-    itemFieldsContainer.removeChild(itemFields[itemIndex]);
-    adjustItemsIndexes();
-    enableOrDisableRemoveButtons();
-    enableOrDisableSubmitButton();
+	var itemFieldsContainer = document.getElementById("itemFields");
+	var itemFields = itemFieldsContainer.children;
+	itemFieldsContainer.removeChild(itemFields[itemIndex]);
+	adjustItemsIndexes();
+	enableOrDisableRemoveButtons();
+	enableOrDisableSubmitButton();
 }
 
 function onType() {
-    enableOrDisableSubmitButton();
+	enableOrDisableSubmitButton();
 }
